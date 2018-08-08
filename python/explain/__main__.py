86cb4a3ed279f8ee79ce8bdd9555b2b0ea1128c2
Erik VanderWerf <evanderwerf@windhoverlabs.com>
2018-08-08 14:32:44 -0500

    Small fix

--- python/explain/__main__.py
+++ python/explain/__main__.py
@@ -8,47 +8,51 @@ from explain.elf_reader import ElfReader
 from explain.map import ElfMap
 
 
-parser = argparse.ArgumentParser(
-    description='Searches an ElfReader database for a symbol.')
-parser.add_argument('--file', help='ELF file from the database')
-# parser.add_argument('--cache')
-parser.add_argument('--database', default=':memory:',
-                    help='use an existing database')
-parser.add_argument('--load', action='store_true',
-                    help='load a new ELF file if an existing database is '
-                         'chosen')
-parser.add_argument('--out', help='output json file')
-parser.add_argument('--print', help='print selected symbol(s)')
-parser.add_argument('--reentrant', action='store_true',
-                    help='ignore any duplicate ELFs')
-symbol = parser.add_mutually_exclusive_group(required=True)
-symbol.add_argument('-a', '--all', action='store_true',
-                    help='output all symbols')
-symbol.add_argument('symbol', nargs='?', help='the symbol name to look at')
+def main():
+    parser = argparse.ArgumentParser(
+        description='Searches an ElfReader database for a symbol.')
+    parser.add_argument('--file', help='ELF file from the database')
+    # parser.add_argument('--cache')
+    parser.add_argument('--database', default=':memory:',
+                        help='use an existing database')
+    parser.add_argument('--load', action='store_true',
+                        help='load a new ELF file if an existing database is '
+                             'chosen')
+    parser.add_argument('--out', help='output json file')
+    parser.add_argument('--print', help='print selected symbol(s)')
+    parser.add_argument('--reentrant', action='store_true',
+                        help='ignore any duplicate ELFs')
+    symbol = parser.add_mutually_exclusive_group(required=True)
+    symbol.add_argument('-a', '--all', action='store_true',
+                        help='output all symbols')
+    symbol.add_argument('symbol', nargs='?', help='the symbol name to look at')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-db = sqlite3.connect(args.database)
-if args.database == ':memory:' or args.load:
-    elf_reader = ElfReader(db)
-    elf_reader.insert_elf(args.file)
+    db = sqlite3.connect(args.database)
+    if args.database == ':memory:' or args.load:
+        elf_reader = ElfReader(db)
+        elf_reader.insert_elf(args.file)
 
-if args.file:
-    elf = ElfMap.from_name(db, args.file)
-    symbols = (elf.symbol(symbol_name=args.symbol),) if not args.all else \
-        elf.symbols()
-else:
-    if args.all:
-        print('Must specify a file with the -a/--all argument.')
-        exit(1)
-    symbol = SymbolMap.from_name(db, args.symbol)
-    elf = symbol.elf
-    symbols = (symbol,)
-if args.print:
-    for symbol in symbols:
-        symbol.print_tree()
+    if args.file:
+        elf = ElfMap.from_name(db, args.file)
+        symbols = (elf.symbol(symbol_name=args.symbol),) if not args.all else \
+            elf.symbols()
+    else:
+        if args.all:
+            print('Must specify a file with the -a/--all argument.')
+            exit(1)
+        symbol = SymbolMap.from_name(db, args.symbol)
+        elf = symbol.elf
+        symbols = (symbol,)
+    if args.print:
+        for symbol in symbols:
+            symbol.print_tree()
 
-if args.out:
-    with open(args.out, 'w') as fp:
-        out = explain_elf(elf, symbols)
-        json.dump(out, fp, indent='  ')
+    if args.out:
+        with open(args.out, 'w') as fp:
+            out = explain_elf(elf, symbols)
+            json.dump(out, fp, indent='  ')
+
+
+main()
