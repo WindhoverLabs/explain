abbd560feba79278d1340133a7b3327ded6d20d2
Mathew Benson <mbenson@windhoverlabs.com>
2018-08-21 04:11:00 +0000

    Split the CMake build functions into two separate functions to support Airliner integration.

--- python/GenerateSymbolMap.cmake
+++ python/GenerateSymbolMap.cmake
@@ -3,7 +3,8 @@ function(explain_generate_symbol_map)
     # Define the function arguments.
     cmake_parse_arguments(PARSED_ARGS "" "INPUT_PATH;INPUT_FILE;DATABASE_NAME;OUTPUT_FILE" "" ${ARGN})
     
-    add_custom_target(CDD_DB ALL
+    add_custom_target(${TARGET_NAME}_DB ALL
+        DEPENDS EXPLAIN_INSTALL
         COMMAND ${PROJECT_SOURCE_DIR}/tools/explain/python/generate_symbol_map 
         	${PARSED_ARGS_INPUT_PATH} 
         	${PARSED_ARGS_INPUT_FILE} 
@@ -11,5 +12,15 @@ function(explain_generate_symbol_map)
         	${PARSED_ARGS_OUTPUT_FILE} 
         	${PROJECT_BINARY_DIR}/host
     )
-    add_dependencies(CDD_DB ${TARGET_NAME})
+    add_dependencies(${TARGET_NAME}_DB ${TARGET_NAME})
 endfunction(explain_generate_symbol_map)
+
+
+function(explain_setup)
+    set(TARGET_NAME ${ARGV0})
+    
+    add_custom_target(EXPLAIN_INSTALL ALL
+        COMMAND ${PROJECT_SOURCE_DIR}/tools/explain/python/setup_explain 
+        	${PROJECT_BINARY_DIR}/host
+    )
+endfunction(explain_setup)
