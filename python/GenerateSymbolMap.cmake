acdcacbd0260b269b2ec47d87d20bc68eab17faf
Luke Doman <ldoman@windhoverlabs.com>
2018-09-11 12:09:47 -0500

    Adding cmake functions and bash scripts

--- python/GenerateSymbolMap.cmake
+++ python/GenerateSymbolMap.cmake
@@ -15,6 +15,35 @@ function(explain_generate_symbol_map)
     add_dependencies(${TARGET_NAME}_DB ${TARGET_NAME})
 endfunction(explain_generate_symbol_map)
 
+function(explain_read_elf)
+    set(TARGET_NAME ${ARGV0})
+    # Define the function arguments.
+    cmake_parse_arguments(PARSED_ARGS "" "INPUT_PATH;DATABASE_NAME" "" ${ARGN})
+    
+    add_custom_target(${TARGET_NAME}_DB ALL
+        DEPENDS EXPLAIN_INSTALL
+        COMMAND ${PROJECT_SOURCE_DIR}/tools/explain/python/read_elf 
+        	${PARSED_ARGS_DATABASE_NAME} 
+        	${PARSED_ARGS_INPUT_PATH}
+        	${PROJECT_BINARY_DIR}/host
+    )
+    add_dependencies(${TARGET_NAME}_DB ${TARGET_NAME})
+    add_dependencies(EXPLAIN_BUILD ${TARGET_NAME}_DB)
+    
+endfunction(explain_generate_symbol_map)
+
+function(explain_generate_cookie)
+    # Define the function arguments.
+    cmake_parse_arguments(PARSED_ARGS "" "DATABASE_NAME;OUTPUT_FILE" "" ${ARGN})
+    
+    add_custom_target(cookie ALL
+        DEPENDS EXPLAIN_INSTALL EXPLAIN_BUILD
+        COMMAND ${PROJECT_SOURCE_DIR}/tools/explain/python/generate_cookie
+        	${PARSED_ARGS_DATABASE_NAME} 
+        	${PARSED_ARGS_OUTPUT_FILE} 
+        	${PROJECT_BINARY_DIR}/host
+    )
+endfunction(explain_generate_symbol_map)
 
 function(explain_setup)
     set(TARGET_NAME ${ARGV0})
