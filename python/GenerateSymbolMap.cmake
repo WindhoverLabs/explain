090652fd9cf20e486bccd91e46bca835104bfb6a
Mathew Benson <mbenson@windhoverlabs.com>
2018-09-20 18:04:15 -0500

    Integrated explain with Airliner build.

--- python/GenerateSymbolMap.cmake
+++ python/GenerateSymbolMap.cmake
@@ -4,7 +4,7 @@ function(explain_generate_symbol_map)
     cmake_parse_arguments(PARSED_ARGS "" "INPUT_PATH;INPUT_FILE;DATABASE_NAME;OUTPUT_FILE" "" ${ARGN})
     
     add_custom_target(${TARGET_NAME}_DB
-        DEPENDS EXPLAIN_INSTALL pyliner_scripting_engine
+        DEPENDS EXPLAIN_INSTALL ground_tools
         COMMAND ${PROJECT_SOURCE_DIR}/tools/explain/python/generate_symbol_map 
         	${PARSED_ARGS_INPUT_PATH} 
         	${PARSED_ARGS_INPUT_FILE} 
@@ -13,13 +13,13 @@ function(explain_generate_symbol_map)
         	${PROJECT_BINARY_DIR}/host
     )
     add_dependencies(${TARGET_NAME}_DB ${TARGET_NAME})
-    add_dependencies(pyliner_scripting_engine ${TARGET_NAME}_DB)
+    add_dependencies(ground_tools ${TARGET_NAME}_DB)
 endfunction(explain_generate_symbol_map)
 
 function(explain_read_elf)
     set(TARGET_NAME ${ARGV0})
     # Define the function arguments.
-    cmake_parse_arguments(PARSED_ARGS "" "INPUT_PATH;DATABASE_NAME" "" ${ARGN})
+    cmake_parse_arguments(PARSED_ARGS "" "INPUT_PATH;DATABASE_NAME" "" ${ARGN})    
     
     add_custom_target(${TARGET_NAME}_DB
         DEPENDS EXPLAIN_INSTALL
@@ -29,8 +29,9 @@ function(explain_read_elf)
         	${PROJECT_BINARY_DIR}/host
     )
     add_dependencies(${TARGET_NAME}_DB ${TARGET_NAME})
-    add_dependencies(pyliner_scripting_engine ${TARGET_NAME}_DB)
+    add_dependencies(ground_tools ${TARGET_NAME}_DB)
     add_dependencies(explain_parsing ${TARGET_NAME}_DB)
+    add_dependencies(explain_cookie ${TARGET_NAME}_DB)
     
 endfunction(explain_read_elf)
 
@@ -38,14 +39,14 @@ function(explain_generate_cookie)
     # Define the function arguments.
     cmake_parse_arguments(PARSED_ARGS "" "DATABASE_NAME;OUTPUT_FILE" "" ${ARGN})
     
-    add_custom_target(cookie
+    add_custom_target(explain_cookie 
         DEPENDS EXPLAIN_INSTALL explain_parsing
         COMMAND ${PROJECT_SOURCE_DIR}/tools/explain/python/generate_cookie
         	${PARSED_ARGS_DATABASE_NAME} 
         	${PARSED_ARGS_OUTPUT_FILE} 
         	${PROJECT_BINARY_DIR}/host
     )
-    add_dependencies(pyliner_scripting_engine cookie)
+    add_dependencies(ground_tools explain_cookie)
 endfunction(explain_generate_cookie)
 
 function(explain_setup)
@@ -53,5 +54,5 @@ function(explain_setup)
         COMMAND ${PROJECT_SOURCE_DIR}/tools/explain/python/setup_explain 
         	${PROJECT_BINARY_DIR}/host
     )
-    add_dependencies(pyliner_scripting_engine EXPLAIN_INSTALL)
+    add_dependencies(ground_tools EXPLAIN_INSTALL)
 endfunction(explain_setup)
