1609d73099ceebfd2a4648159a8d831d18f74967
Mathew Benson <mbenson@windhoverlabs.com>
2018-10-17 04:50:38 +0000

    Updated cmake so it supports core-builder also

--- python/GenerateSymbolMap.cmake
+++ python/GenerateSymbolMap.cmake
@@ -6,14 +6,16 @@ function(explain_generate_symbol_map)
     add_custom_target(${TARGET_NAME}_DB
         DEPENDS EXPLAIN_INSTALL ground_tools
         COMMAND ${PROJECT_SOURCE_DIR}/tools/explain/python/generate_symbol_map 
-        	${PARSED_ARGS_INPUT_PATH} 
-        	${PARSED_ARGS_INPUT_FILE} 
-        	${PARSED_ARGS_DATABASE_NAME} 
-        	${PARSED_ARGS_OUTPUT_FILE} 
-        	${PROJECT_BINARY_DIR}/host
+            ${PARSED_ARGS_INPUT_PATH} 
+            ${PARSED_ARGS_INPUT_FILE} 
+            ${PARSED_ARGS_DATABASE_NAME} 
+            ${PARSED_ARGS_OUTPUT_FILE} 
+            ${PROJECT_BINARY_DIR}/host
     )
     add_dependencies(${TARGET_NAME}_DB ${TARGET_NAME})
-    add_dependencies(ground_tools ${TARGET_NAME}_DB)
+    if(TARGET ground_tools)
+        add_dependencies(ground_tools ${TARGET_NAME}_DB)
+    endif(TARGET ground_tools)
 endfunction(explain_generate_symbol_map)
 
 function(explain_read_elf)
@@ -24,12 +26,14 @@ function(explain_read_elf)
     add_custom_target(${TARGET_NAME}_DB
         DEPENDS EXPLAIN_INSTALL
         COMMAND ${PROJECT_SOURCE_DIR}/tools/explain/python/read_elf 
-        	${PARSED_ARGS_DATABASE_NAME} 
-        	${PARSED_ARGS_INPUT_PATH}
-        	${PROJECT_BINARY_DIR}/host
+            ${PARSED_ARGS_DATABASE_NAME} 
+            ${PARSED_ARGS_INPUT_PATH}
+            ${PROJECT_BINARY_DIR}/host
     )
     add_dependencies(${TARGET_NAME}_DB ${TARGET_NAME})
-    add_dependencies(ground_tools ${TARGET_NAME}_DB)
+    if(TARGET ground_tools)
+        add_dependencies(ground_tools ${TARGET_NAME}_DB)
+    endif(TARGET ground_tools)
     add_dependencies(explain_parsing ${TARGET_NAME}_DB)
     add_dependencies(explain_cookie ${TARGET_NAME}_DB)
     
@@ -42,17 +46,21 @@ function(explain_generate_cookie)
     add_custom_target(explain_cookie 
         DEPENDS EXPLAIN_INSTALL explain_parsing
         COMMAND ${PROJECT_SOURCE_DIR}/tools/explain/python/generate_cookie
-        	${PARSED_ARGS_DATABASE_NAME} 
-        	${PARSED_ARGS_OUTPUT_FILE} 
-        	${PROJECT_BINARY_DIR}/host
+            ${PARSED_ARGS_DATABASE_NAME} 
+            ${PARSED_ARGS_OUTPUT_FILE} 
+            ${PROJECT_BINARY_DIR}/host
     )
-    add_dependencies(ground_tools explain_cookie)
+    if(TARGET ground_tools)
+        add_dependencies(ground_tools explain_cookie)
+    endif(TARGET ground_tools)
 endfunction(explain_generate_cookie)
 
 function(explain_setup)
     add_custom_target(EXPLAIN_INSTALL
         COMMAND ${PROJECT_SOURCE_DIR}/tools/explain/python/setup_explain 
-        	${PROJECT_BINARY_DIR}/host
+            ${PROJECT_BINARY_DIR}/host
     )
-    add_dependencies(ground_tools EXPLAIN_INSTALL)
+    if(TARGET ground_tools)
+        add_dependencies(ground_tools EXPLAIN_INSTALL)
+    endif(TARGET ground_tools)
 endfunction(explain_setup)
